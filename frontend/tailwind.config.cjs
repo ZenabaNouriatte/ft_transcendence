8dbb0546ac57e2137fed4cebb874b5ed8eca3482
Nessie <114988574+0x2Anessie@users.noreply.github.com>
2025-11-09 15:33:02 +0100

    commentaires front

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,6 +1,16 @@
+// CONFIGURATION DE TAILWIND CSS
+
+// Permet d'avoir l'autocomplétion dans vscode
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
+  // Liste des fichiers à scanner pour trouver les classes Tailwind utilisées
   content: ['./index.html', './src/**/*.{ts,tsx,js,jsx}'],
+
+  // Normalement pour personnaliser le thème Tailwind (couleurs, polices, etc)
+  // Ici on garde les valeurs par défaut
   theme: { extend: {} },
+
+  // Pas de plugins utilisés
   plugins: [],
 };
